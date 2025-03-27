78bbe89dade362fdcd3c32c5ffb005b2e4f6c0ba
Tiiita <tiiitamail@gmail.com>
2025-03-27 22:54:22 +0100

    Add first structure

---
+++ src/client.rs
@@ -0,0 +1,12 @@
+use crate::AppicationConfig;
+
+pub struct Client<'a> {
+    pub app_conf: AppicationConfig<'a>
+}
+
+
+impl<'a> Client<'a> {
+    pub fn connect() {
+        
+    }    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env::var;
+
+use client::Client;
+mod client;
+pub struct AppicationConfig<'a> {
+    pub oauth_token: &'a str,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let token = var("OAUTH").expect("Unable to find OAUTH in env");
+    let app_conf = AppicationConfig {
+        oauth_token: &token
+    };
+
+    let _client = Client { app_conf: app_conf };
 }
